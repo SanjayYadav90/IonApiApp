7cedb4bcbdb3e81bf11c56e981adc2de934ae7bd
Sanjay Yadav <sanjaykumar5c9@yahoo.com>
2016-10-29 21:36:33 +0530

    this is initial commit of the app.

---
+++ src/pages/home/home.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {PeopleService} from '../../providers/people-service';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  providers: [PeopleService],
+  templateUrl: 'home.html'
+})
+
+
+export class HomePage {
+  public people: any;
+
+  constructor( public navCtrl: NavController, public peopleService: PeopleService){
+    this.loadPeople();
+  }
+
+  loadPeople(){
+    this.peopleService.load()
+    .then(data => {
+      this.people = data;
+    });
+  }
+}
+
+
+// export class HomePage {
+
+//   public people: any;
+//   constructor( public navCtrl: NavController, public peopleService: PeopleService) {
+    
+//   }
+
+//   loadPeople(){
+//   	this.peopleService.load()
+//   	.then(data => {
+//   	  this.people = data;
+//   	});
+//   }
+// }
